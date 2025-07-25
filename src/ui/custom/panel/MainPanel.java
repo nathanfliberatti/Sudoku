5f8885fefa93ce426ca65b220db19d3431b902bd
nathanfliberatti <nathan.liberatti@gmail.com>
2025-07-24 22:09:53 -0300

    Etapa 1 - adicionando interface gráfica no Sudoku

---
+++ src/ui/custom/panel/MainPanel.java
@@ -0,0 +1,13 @@
+package ui.custom.panel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainPanel extends JPanel {
+
+    public MainPanel(final Dimension dimension) {
+        this.setSize(dimension);
+        this.setPreferredSize(dimension);
+    }
+
+}
