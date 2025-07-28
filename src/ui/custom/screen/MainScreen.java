2acec3e29aa0dc37fb9555c111b25997bd61dcd4
nathanfliberatti <nathan.liberatti@gmail.com>
2025-07-27 21:46:05 -0300

    Etapa 2 - Construindo os componentes do projeto

---
+++ src/ui/custom/screen/MainScreen.java
@@ -0,0 +1,85 @@
+package ui.custom.screen;
+
+import service.BoardService;
+import ui.custom.button.CheckGameStatusButton;
+import ui.custom.button.FinishGameButton;
+import ui.custom.button.ResetButton;
+import ui.custom.frame.MainFrame;
+import ui.custom.panel.MainPanel;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Map;
+
+import static javax.swing.JOptionPane.QUESTION_MESSAGE;
+import static javax.swing.JOptionPane.YES_NO_OPTION;
+
+public class MainScreen {
+
+    private final static Dimension dimension = new Dimension(600, 600);
+
+    private final BoardService boardService;
+
+    private JButton resetButton;
+    private JButton checkGameStatusButton;
+    private JButton finishGameButton;
+
+    public MainScreen(final Map<String, String> gameConfig) {
+        this.boardService = new BoardService(gameConfig);
+    }
+
+    public void buildMainScreen() {
+        JPanel mainPanel = new MainPanel(dimension);
+        JFrame mainFrame = new MainFrame(dimension, mainPanel);
+        addResetButton(mainPanel);
+        addCheckGameStatusButton(mainPanel);
+        addFinishGameButton(mainPanel);
+        mainFrame.revalidate();
+        mainFrame.repaint();
+    }
+
+    private void addResetButton(final JPanel mainPanel) {
+        resetButton = new ResetButton(e -> {
+            var dialogResult = JOptionPane.showConfirmDialog(
+                    null,
+                    "Deseja realmente reiniciar o jogo?",
+                    "Limpar o jogo",
+                    YES_NO_OPTION,
+                    QUESTION_MESSAGE
+            );
+            if (dialogResult == 0) {
+                boardService.reset();
+            }
+        });
+        mainPanel.add(resetButton);
+    }
+
+    private void addCheckGameStatusButton(JPanel mainPanel) {
+        checkGameStatusButton = new CheckGameStatusButton(e -> {
+            var hasErrors = boardService.hasErrors();
+            var gameStatus = boardService.getStatus();
+            var message = switch (gameStatus) {
+                case NON_STARTED -> "O jogo não foi iniciado";
+                case INCOMPLETE -> "O jogo está incompleto";
+                case COMPLETE -> "O jogo está completo";
+            };
+            message += hasErrors ? " e contém erros" : " e não contém erros";
+            JOptionPane.showMessageDialog(null, message);
+        });
+        mainPanel.add(checkGameStatusButton);
+    }
+
+    private void addFinishGameButton(final JPanel mainPanel) {
+        finishGameButton = new FinishGameButton(e -> {
+            if (boardService.gameIsFinished()) {
+                JOptionPane.showMessageDialog(null, "Parabéns, você concluiu o jogo");
+                resetButton.setEnabled(false);
+                checkGameStatusButton.setEnabled(false);
+                finishGameButton.setEnabled(false);
+            } else {
+                JOptionPane.showMessageDialog(null, "Seu jogo tem alguma inconsistência, ajuste e tente novamente");
+            }
+        });
+        mainPanel.add(finishGameButton);
+    }
+}
